8e0055cf4bee4b130736b00a0b8140212f8b8ad5
samuelvela7 <samuelvela7@hotmail.com>
2022-07-12 14:29:03 -0500

    se hizo una modificacion 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-               String meessage= "Default message, practica 1";
+               String meessage= "Default message, prueba 2";
                Assert.assertNotNull(meessage);
 	}
 
